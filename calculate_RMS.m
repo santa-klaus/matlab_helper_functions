f6cf2a9249a60686c9803c4ab8e443a10b58166d
santa-klaus <10899463+santa-klaus@users.noreply.github.com>
2020-02-03 16:31:24 +0100

    Add calculate_RMS wip

---
+++ calculate_RMS.m
@@ -0,0 +1,22 @@
+%% calculate_volt_strength_ind
+% by Paul Imgart, last change
+% Function to calculate RMS from variable time step data,
+% e.g. simulation data
+%   
+% Inputs:
+% t_stamp: Time stamps in s
+% sig: Signal
+% t_length: Step width over which the RMS should be computed
+%
+% Outputs:
+% rms_sig: RMS values of the signal
+%
+%
+% Include downsampling function?
+
+function [rms_sig] = calculate_RMS(t_stamp,sig,t_length)
+
+
+
+
+end
